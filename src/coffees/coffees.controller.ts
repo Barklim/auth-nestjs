0d46f97c7ab1a0afa7b56913f3833a24b33263f8
Barklim <46284548+Barklim@users.noreply.github.c>
2023-09-19 04:07:13 +0800

    feat: active user decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { ActiveUser } from '../iam/decorators/active-user.decorator';
+import { ActiveUserData } from '../iam/interfaces/active-user-data.interface';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -13,7 +15,8 @@ export class CoffeesController {
   }
 
   @Get()
-  findAll() {
+  findAll(@ActiveUser() user: ActiveUserData) {
+    console.log(user);
     return this.coffeesService.findAll();
   }
 
