b9049d68185936e222ee30e9b542f2cda5d1ae87
Barklim <46284548+Barklim@users.noreply.github.c>
2023-09-19 03:57:16 +0800

    feat: public route

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -10,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
 import jwtConfig from './config/jwt.config'
 import { APP_GUARD } from '@nestjs/core';
 import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
+import { AuthenticationGuard } from './authentication/guards/authentication/authentication.guard';
 
 @Module({
   imports: [
@@ -24,8 +25,9 @@ import { AccessTokenGuard } from './authentication/guards/access-token/access-to
     },
     {
       provide: APP_GUARD,
-      useClass: AccessTokenGuard
+      useClass: AuthenticationGuard
     },
+    AccessTokenGuard,
     AuthenticationService,
   ],
   controllers: [AuthenticationController]
